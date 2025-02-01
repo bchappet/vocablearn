45058129191e9d24f4dbdd246bddf447db112484
Benoit Chappet <bdevangel@gmail.com>
2025-02-01 23:29:21 +0100

    Updated settings (with no settings available)

--- routers/settings.py
+++ routers/settings.py
@@ -9,30 +9,11 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/settings", response_class=HTMLResponse)
 async def get_settings(request: Request, db: SessionDep):
-    statement = select(Settings)
-    settings = db.exec(statement).first()
     return templates.TemplateResponse(
         "settings.html",
-        {"request": request, "settings": settings}
+        {"request": request}
     )
 
 @router.post("/settings")
-async def update_settings(
-    db: SessionDep,
-    direction: str = Form(...),
-    questions_per_quiz: int = Form(...),
-):
-    # Delete all existing settings
-    statement = select(Settings)
-    existing_settings = db.exec(statement).all()
-    for setting in existing_settings:
-        db.delete(setting)
-    db.commit()
-    
-    # Create new settings
-    settings = Settings(
-        nb_questions=questions_per_quiz
-    )
-    db.add(settings)
-    db.commit()
+async def update_settings():
     return RedirectResponse("/", status_code=303)
